e1533ec666a72f2705087dcca009dc290fb72e46
Ali abbas <94835254+aliabbasmaryami@users.noreply.github.com>
2023-03-04 15:55:45 +0500

    Add files via upload

---
+++ practice.java/js/script.js
@@ -0,0 +1,26 @@
+// for lopo in javascript
+let sum = 0;
+let n = prompt("Enter value of n")
+n = Number.parseInt(n)
+for(let i=0; i<n; i++){
+    sum += (i+1)
+    // console.log(i+1)
+}
+console.log("sum of first" + n + "are natural number" + sum );
+console.log(i)
+// for in loop in javascript
+// let obj = {
+//     ali: 80,
+//     tanvver: 70,
+//     Haider: 90,
+//     buti: 40
+// }
+// for(let a in obj){
+//     console.log("marks of" + a + "are" + obj[a])
+// }
+
+
+// // for of loop in javascript
+// for(let b of "aliabbas"){
+//     console.log(b)
+// }
